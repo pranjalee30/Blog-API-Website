abc59e5396458369486d17b5e7dbcfdbd74b249d
Pranjal Kumar <pranjalk449@gmail.com>
2024-08-17 02:38:47 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // In-memory data store
 let posts = [
